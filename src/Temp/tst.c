02777596abb53f36ca8db28cd704598cbf04dfcd
max-desousa <max.de.sousa@outlook.com>
2022-07-06 20:07:32 -0400

    Adding test files to see how CMAKE fetchContent
    works in my project specific repo

---
+++ src/Temp/tst.c
@@ -0,0 +1,6 @@
+#include "tst.h"
+
+void DoSomethingOnTerminal()
+  {
+  printf("I'm printing something on the terminal... from inside the library that you made and downloaded via cmake... \n\ngood job!\n\n");
+  }
---
+++ src/Temp/tst.h
@@ -0,0 +1,9 @@
+#ifndef TST_HEADER
+#define TST_HEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void DoSomethingOnTerminal();
+
+#endif
