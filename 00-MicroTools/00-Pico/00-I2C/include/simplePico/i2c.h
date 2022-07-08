9fc935acccc120b0b628595458e5349b0891fb2d
Max DeSousa <maxdesousa@protonmail.com>
2022-07-08 02:20:57 -0400

    I created a simple library at the bar... but it's not working
    I can't get my project - when importing these CMake files
    to recognize my #include of simplePico/i2c.h ... and I have
    no idea why. Commiting to try and assess on my desktop with
    more tools

--- 00-MicroTools/00-Pico/00-I2C/include/simplePico/i2c.h
+++ 00-MicroTools/00-Pico/00-I2C/include/simplePico/i2c.h
@@ -3,6 +3,9 @@
 
 #include "stdint.h"
 #include "stdio.h"
+#include "hardware/i2c.h"
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
 
 void SetupI2cBus(uint8_t _sdaPin, uint8_t _sclPin, uint32_t _baudRate);
 
