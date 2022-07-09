228f170a2a38d6ab0a3c0b30cf5ec2e2410dba3a
Max DeSousa <maxdesousa@protonmail.com>
2022-07-09 16:09:04 -0400

    Cleaning up my cmake, and starting a legin I2C pico lib
    also removed my name... i don't want it to be "max desousa's
    blah blah blah"... I want it to stand on its own...

--- 00-MicroTools/00-Pico/00-I2C/i2c.c
+++ 00-MicroTools/00-Pico/00-I2C/i2c.c
@@ -1,6 +1,10 @@
 #include "simplePico/i2c.h"
 
-void SetupI2cBus(uint8_t _sdaPin, uint8_t _sclPin, uint32_t _baudRate)
+uint8_t SetupI2cBus(i2c_inst_t *i2c, uint8_t _sdaPin, uint8_t _sclPin, uint32_t _baudRate)
   {
-  printf("Placeholder for when I can actually build the function);
+  gpio_set_function(_sdaPin, GPIO_FUNC_I2C);
+  gpio_set_function(_sclPin, GPIO_FUNC_I2C);
+  gpio_pull_up(_sdaPin);
+  gpio_pull_up(_sclPin);
+  i2c_init(i2c, _baudRate);
   }
--- 00-MicroTools/00-Pico/00-I2C/include/simplePico/i2c.h
+++ 00-MicroTools/00-Pico/00-I2C/include/simplePico/i2c.h
@@ -3,7 +3,13 @@
 
 #include "stdint.h"
 #include "stdio.h"
+#include "hardware/i2c.h"
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
 
-void SetupI2cBus(uint8_t _sdaPin, uint8_t _sclPin, uint32_t _baudRate);
+//bool I2c0BusInUse = false;
+//bool I2c1BusInUse = false;
+
+uint8_t SetupI2cBus(i2c_inst_t *i2c, uint8_t _sdaPin, uint8_t _sclPin, uint32_t _baudRate);
 
 #endif
