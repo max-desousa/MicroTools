d7a0ec2011dc3344584aadcaaefb4563b0224aa3
Max DeSousa <maxdesousa@protonmail.com>
2022-07-15 01:22:07 -0400

    Nothing special commit from light bar work
    Notable change is the creation of a struct to treat the c library
    as OO. Otherwise some minor changes - didn't end up having internet
    access here and I think the docs folder is missing a file
    that lets me know the steps to configure the module to accomplish
    certain functions.

--- 01-Modules/00-GPS/00-ZEDF9P/include/gps/zedf9p.h
+++ 01-Modules/00-GPS/00-ZEDF9P/include/gps/zedf9p.h
@@ -6,12 +6,60 @@
 #include "stdbool.h"
 #include "simplePico/i2c.h"
 
+
+
+/* ========== Begin enums ================== */
+
 typedef enum
   {
   i2c,
   spi
-  } commsMethod;
+  } T_ZF9P_e_CommsMethod;
 
-bool SimpleSetupZedf9p(commsMethod _method, void *_commsSetup);
+typedef enum
+  {
+  UBX_RTCM_1005,
+  UBX_RTCM_1074,
+  UBX_RTCM_1084,
+  UBX_RTCM_1094,
+  UBX_RTCM_1124,
+  UBX_RTCM_1230
+  } T_ZF9P_e_Setting;
 
+/* ========== End enums ================== */
+
+
+
+/* ========== Begin Structures ================== */
+
+/* This is a c library but we'll go about it is a relatively object oriented 
+ * fasion */
+typedef struct T_ZF9P_s_ModuleConfig 
+  {
+  T_ZF9P_e_CommsMethod commsMethod;
+  uint8_t sdaPin;
+  uint8_t clockPin;
+  uint8_t misoPin;
+  uint8_t mosiPin;
+  uint8_t chipSelectPin;
+  uint32_t baudRate;
+  } T_ZF9P_s_ModuleConfig;
+
+/* ========== End Structures ================== */
+
+
+
+/* =============== Begin Functions ================ */
+
+/* Different Simplified Setups */
+bool SimpleSetupZedf9p(T_ZF9P_s_ModuleConfig *_moduleConfig);
+bool SimpleSetupSurveyInBase(T_ZF9P_s_ModuleConfig *_moduleConfig, uint32_t _time, uint32_t _accuracy);
+
+/* Functions that refactor common/series-of commands */
+void SetUbxBaseSettings();
+
+/* Lower level module functions */
+void SetUbxZedf9pSetting(T_ZF9P_e_Setting _setting, uint32_t _value);
+
+/* ============= End Functions ===================== */
 #endif
--- 01-Modules/00-GPS/00-ZEDF9P/zedf9p.c
+++ 01-Modules/00-GPS/00-ZEDF9P/zedf9p.c
@@ -1,18 +1,16 @@
 #include "gps/zedf9p.h"
 
-bool SimpleSetupZedf9p(commsMethod _method, void *_commsSetup)
+bool SimpleSetupZedf9p(T_ZF9P_s_ModuleConfig *_moduleConfig)
   {
   bool retVal;
   uint32_t actualBaudRate = 0;
   uint32_t desiredBaudRate = 0;
 
-  switch (_method)
+  switch (_moduleConfig->commsMethod)
     {
     case i2c:
-      ;
-      i2c_struct* i2c_setup;
-      actualBaudRate = SetupI2cBus(i2c_setup->dataPin, i2c_setup->clockPin, i2c_setup->baudRate);
-      desiredBaudRate = i2c_setup->baudRate;
+      actualBaudRate = SetupI2cBus(_moduleConfig->sdaPin, _moduleConfig->clockPin, _moduleConfig->baudRate);
+      desiredBaudRate = _moduleConfig->baudRate;
       break;
     case spi:
       retVal = false;
@@ -29,4 +27,28 @@ bool SimpleSetupZedf9p(commsMethod _method, void *_commsSetup)
     retVal = true;
     }
   return retVal;
+  }
+
+bool SimpleSetupSurveyInBase(T_ZF9P_s_ModuleConfig *_moduleConfig, uint32_t _time, uint32_t _accuracy)
+  {
+  /* First we do the setup of the module on the respective comms bus */
+  if(!SimpleSetupZedf9p(_moduleConfig))
+    {
+    return false;
+    }
+  SetUbxBaseSettings();
+  }
+
+void SetUbxBaseSettings()
+  {
+  SetUbxZedf9pSetting(UBX_RTCM_1005, 1);
+  SetUbxZedf9pSetting(UBX_RTCM_1074, 1);
+  SetUbxZedf9pSetting(UBX_RTCM_1084, 1);
+  SetUbxZedf9pSetting(UBX_RTCM_1094, 1);
+  SetUbxZedf9pSetting(UBX_RTCM_11124, 1);
+  SetUbxZedf9pSetting(UBX_RTCM_1230, 10);
+  }
+
+void SetUbxZedf9pSetting(T_ZF9P_e_Setting _setting, uint32_t _value)
+  {
   }
